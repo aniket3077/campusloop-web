6d287ec56039a2770ef117924457a6c026cc2fb4
aniket3077 <bankaraniketk@gmail.com>
2026-09-04 02:03:24 +0530

    feat: add firebase project configuration file

---
+++ src/config/firebase.ts
@@ -0,0 +1,10 @@
+// Firebase Web Configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyDgHer4ZoxaBa2hGIuDnABQLNlwndwlwkw",
+  authDomain: "campus-loop-430ab.firebaseapp.com",
+  projectId: "campus-loop-430ab",
+  storageBucket: "campus-loop-430ab.firebasestorage.app",
+  messagingSenderId: "183123201021",
+  appId: "1:183123201021:web:33d63a6b39adb7d6736794",
+  measurementId: "G-ZQHHFM6T3V"
+};
